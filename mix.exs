dacb88e2c5db8c022514d50fe0a806204cb41a73
tsai <jackalcooper@gmail.com>
2022-12-28 02:51:55 +0800

    v0.2.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kinda.MixProject do
   def project do
     [
       app: :kinda,
-      version: "0.2.9",
+      version: "0.2.10",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
