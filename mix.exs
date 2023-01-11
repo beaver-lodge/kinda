2cfde11440f46c8237754fd792e420a6f8ad8897
tsai <jackalcooper@gmail.com>
2023-01-11 23:04:20 +0800

    rm def array

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kinda.MixProject do
   def project do
     [
       app: :kinda,
-      version: "0.2.10",
+      version: "0.2.11",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
