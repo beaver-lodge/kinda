c9f544d728667c093c420a9264449c172cd55d08
tsai <jackalcooper@gmail.com>
2023-01-12 22:37:05 +0800

    add

--- lib/resource_kind.ex
+++ lib/resource_kind.ex
@@ -2,35 +2,23 @@ defmodule Kinda.ResourceKind do
   defmacro __using__(opts) do
     forward_module = Keyword.fetch!(opts, :forward_module)
     fields = Keyword.get(opts, :fields) || []
-    generic = Keyword.get(opts, :generic) || false
     gen_spec = Keyword.get(opts, :gen_spec, true)
 
     quote bind_quoted: [
             forward_module: forward_module,
             fields: fields,
-            generic: generic,
             gen_spec: gen_spec
           ] do
-      kind = if generic, do: nil, else: __MODULE__
-      defstruct [ref: nil, bag: MapSet.new(), kind: kind] ++ fields
+      defstruct [ref: nil, bag: MapSet.new()] ++ fields
 
       if gen_spec do
         @type t() :: %__MODULE__{}
       end
 
-      if generic do
-        def make(kind, args) when is_atom(kind) and is_list(args) do
-          %__MODULE__{
-            ref: unquote(forward_module).forward(kind, "make", args),
-            kind: kind
-          }
-        end
-      else
-        def make(value) do
-          %__MODULE__{
-            ref: unquote(forward_module).forward(__MODULE__, "make", [value])
-          }
-        end
+      def make(value) do
+        %__MODULE__{
+          ref: unquote(forward_module).forward(__MODULE__, "make", [value])
+        }
       end
     end
   end
