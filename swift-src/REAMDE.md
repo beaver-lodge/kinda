6f4de85e0b27c5100c5bd4c93192b22716602640
tsai <jackalcooper@gmail.com>
2023-06-19 15:48:39 +0800

    add

--- swift-src/REAMDE.md
+++ swift-src/REAMDE.md
@@ -7,5 +7,5 @@ docker run --rm -it -v $PWD/swift-src:/swift-src swiftlang/swift:nightly-5.9-foc
 ```
 
 ```bash
-docker run --rm -it -v $PWD/swift-src:/swift-src -v $HOME/oss/swift-cmake-examples:/swift-cmake-examples -w /swift-cmake-examples/3_bidirectional_cxx_interop jackalcooper/kinda-dev-llvm-17-arm64 bash /swift-src/cmake.sh
+docker run --rm -it -v $PWD/swift-src:/swift-src -v $HOME/oss/swift-cmake-examples:/swift-cmake-examples -w /swift-cmake-examples jackalcooper/kinda-dev-llvm-17-arm64 bash /swift-src/cmake.sh
 ```
--- swift-src/cmake.sh
+++ swift-src/cmake.sh
@@ -1,7 +1,11 @@
 set -ex
-mkdir -p build
-cd build
-cmake -DCMAKE_CXX_COMPILER="clang++" -G 'Ninja' ../
-ninja
-./src/fibonacci_cpp
-./src/fibonacci_swift
+cmake \
+  -DCMAKE_C_COMPILER="clang"  -DCMAKE_CXX_COMPILER="clang++" \
+  -B 3_bidirectional_cxx_interop/build \
+  -G 'Ninja' \
+  -DLLVM_DIR=/usr/lib/llvm-17/lib/cmake/llvm \
+  -DMLIR_DIR=/usr/lib/llvm-17/lib/cmake/mlir \
+  3_bidirectional_cxx_interop
+cmake --build 3_bidirectional_cxx_interop/build
+./3_bidirectional_cxx_interop/build/src/fibonacci_cpp
+./3_bidirectional_cxx_interop/build/src/fibonacci_swift
