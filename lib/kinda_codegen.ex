27a32893d026586657ad34da25c45608b4db3cc5
tsai <jackalcooper@gmail.com>
2024-02-21 16:00:05 +0800

    simplify code gen

--- lib/kinda_codegen.ex
+++ lib/kinda_codegen.ex
@@ -3,14 +3,67 @@ defmodule Kinda.CodeGen do
   Behavior for customizing your source code generation.
   """
 
-  alias Kinda.CodeGen.{KindDecl}
+  alias Kinda.CodeGen.{KindDecl, NIFDecl}
 
-  defmacro __using__(_) do
+  defmacro __using__(opts) do
     quote do
-      @behaviour Kinda.CodeGen
+      mod = unquote(opts)[:with]
+
+      Kinda.CodeGen.nif_ast(mod.kinds(), mod.nifs(), Beaver.MLIR.CAPI, Beaver.Native)
+      |> then(&Module.eval_quoted(__MODULE__, &1))
     end
   end
 
   @callback kinds() :: KindDecl.t()
   def kinds(), do: []
+
+  def nif_ast(kinds, nifs, root_module, forward_module) do
+    # generate stubs for generated NIFs
+    extra_kind_nifs =
+      kinds
+      |> Enum.map(&NIFDecl.from_resource_kind/1)
+      |> List.flatten()
+
+    for nif <- nifs ++ extra_kind_nifs do
+      nif =
+        case nif do
+          {wrapper_name, arity} when is_atom(wrapper_name) and is_integer(arity) ->
+            %NIFDecl{
+              wrapper_name: wrapper_name,
+              nif_name: Module.concat(root_module, wrapper_name),
+              arity: arity
+            }
+
+          %NIFDecl{} ->
+            nif
+        end
+
+      args_ast = Macro.generate_unique_arguments(nif.arity, __MODULE__)
+
+      %NIFDecl{wrapper_name: wrapper_name, nif_name: nif_name} = nif
+
+      wrapper_name =
+        if is_bitstring(wrapper_name) do
+          String.to_atom(wrapper_name)
+        else
+          wrapper_name
+        end
+
+      quote do
+        @doc false
+        def unquote(nif_name)(unquote_splicing(args_ast)),
+          do:
+            raise(
+              "NIF for resource kind is not implemented, or failed to load NIF library. Function: :\"#{unquote(nif_name)}\"/#{unquote(nif.arity)}"
+            )
+
+        def unquote(wrapper_name)(unquote_splicing(args_ast)) do
+          refs = Kinda.unwrap_ref([unquote_splicing(args_ast)])
+          ret = apply(__MODULE__, unquote(nif_name), refs)
+          unquote(forward_module).check!(ret)
+        end
+      end
+    end
+    |> List.flatten()
+  end
 end
--- lib/zig_precompiler.ex
+++ lib/zig_precompiler.ex
@@ -12,11 +12,6 @@ defmodule Kinda.Precompiler do
 
   @impl true
   def build_native(args) do
-    config = Mix.Project.config()
-    app = config |> Keyword.fetch!(:app)
-    version = Mix.Project.config() |> Keyword.fetch!(:version)
-    {:ok, t} = current_target()
-    System.put_env("KINDA_LIB_NAME", "#{app}-v#{version}-#{t}")
     ElixirMake.Compiler.compile(args)
   end
 
