bac30b83b20b74afad1f2c1a989a7c508c6925c1
tsai <jackalcooper@gmail.com>
2024-04-29 17:24:44 +0800

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,12 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    const test_step = b.step("test", "Test it");
-    b.default_step = test_step;
-
-    const optimize: std.builtin.OptimizeMode = .Debug;
-    _ = optimize;
-
     const erl_nif = b.addModule(
         "erl_nif",
         .{ .root_source_file = .{ .path = "src/erl_nif.zig" } },
