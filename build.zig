1382e8b5a1737369192f1ba6538e501a5e665aa3
tsai <jackalcooper@gmail.com>
2024-06-13 15:37:21 +0800

    update for zig 0.13

--- build.zig
+++ build.zig
@@ -3,16 +3,16 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const erl_nif = b.addModule(
         "erl_nif",
-        .{ .root_source_file = .{ .path = "src/erl_nif.zig" } },
+        .{ .root_source_file = b.path("src/erl_nif.zig") },
     );
     const beam = b.addModule(
         "beam",
-        .{ .root_source_file = .{ .path = "src/beam.zig" } },
+        .{ .root_source_file = b.path("src/beam.zig") },
     );
     beam.addImport("erl_nif", erl_nif);
     const kinda = b.addModule(
         "kinda",
-        .{ .root_source_file = .{ .path = "src/kinda.zig" } },
+        .{ .root_source_file = b.path("src/kinda.zig") },
     );
     kinda.addImport("erl_nif", erl_nif);
     kinda.addImport("beam", beam);
