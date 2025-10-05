8718d7dea3e5cf42f5ce4348b4268b40b7fc583d
Shenghang Tsai <jackalcooper@gmail.com>
2025-10-05 15:39:59 +0800

    Update result.zig

--- src/result.zig
+++ src/result.zig
@@ -2,14 +2,18 @@ const beam = @import("beam");
 const e = @import("erl_nif").c;
 const std = @import("std");
 
+pub fn is_stack_trace_enabled() bool {
+    var value: [256]u8 = undefined;
+    var value_size: usize = value.len;
+    return e.enif_getenv("KINDA_DUMP_STACK_TRACE", &value[0], &value_size) != 0;
+}
+
 pub fn nif_with_flags(comptime name: [*c]const u8, comptime arity: usize, comptime f: anytype, comptime flags: u32) type {
     const ns = "Elixir.";
     return struct {
         fn exported(env: beam.env, n: c_int, args: [*c]const beam.term) callconv(.c) beam.term {
             return f(env, n, args) catch |err| {
-                var value: [256]u8 = undefined;
-                var value_size: usize = value.len;
-                if (e.enif_getenv("KINDA_DUMP_STACK_TRACE", &value[0], &value_size) == 0) {
+                if (is_stack_trace_enabled()) {
                     std.debug.dumpStackTrace(@errorReturnTrace().*);
                 }
                 return beam.raise_exception(env, ns ++ "Kinda.CallError", err);
