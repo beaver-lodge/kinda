49794dcf880e2193ec377c8a47d21c4ec47a171e
Shenghang Tsai <jackalcooper@gmail.com>
2026-08-10 09:40:06 +0800

    ci: enforce Credo and warning-free builds

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule Kinda.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:elixir_make, "~> 0.4", runtime: false},
       {:jason, "~> 1.4"}
